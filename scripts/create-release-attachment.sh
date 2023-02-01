85eac2e7a9a1c48399abf4dc0f55c5ede13277a8
Andrea Stagi <stagi.andrea@gmail.com>
2023-02-01 15:18:15 +0100

    chore(ci): add actions

---
+++ scripts/create-release-attachment.sh
@@ -0,0 +1,5 @@
+mkdir -p ./design-tokens-italia
+cp -r css ./design-tokens-italia
+cp -r scss ./design-tokens-italia
+cp -r tokens ./design-tokens-italia
+zip -r ./design-tokens-italia.zip ./design-tokens-italia
